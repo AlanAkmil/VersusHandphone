614c072d3a6287be5814e6fdef39ad0c54958ddd
AlanAkmil <alansubagyo058@gmail.com>
2026-08-21 06:48:11 +0700

    update app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Header from "@/components/Header";
 import SearchHero from "@/components/SearchHero";
+import QuickCompareForm from "@/components/QuickCompareForm";
 import { mockSuggestions } from "@/data/mock-duel";
 
 export default function Home() {
@@ -7,6 +8,9 @@ export default function Home() {
     <main className="min-h-screen bg-void">
       <Header />
       <SearchHero suggestions={mockSuggestions} />
+      <div className="px-6 pb-20">
+        <QuickCompareForm />
+      </div>
     </main>
   );
 }
